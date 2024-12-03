e03d3a55ba99cbd7876bc238594c1a1d2f5e618f
Evan Lei. <71707767+EvanLei-git@users.noreply.github.com>
2024-12-03 11:29:47 +0200

    Create test
    SpringBoot
    
    deleting test

---
+++ src/test/java/gr/hua/dit/rentalapp/RentalappApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.rentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
