158303f3bcdf80937d2df47e9664065ed200d736
harris <harristriantis@gmail.com>
2024-12-21 17:19:34 +0200

    new services upload

--- src/main/java/gr/hua/dit/rentalapp/repositories/PropertyVisitRepository.java
+++ src/main/java/gr/hua/dit/rentalapp/repositories/PropertyVisitRepository.java
@@ -1,9 +1,11 @@
 package gr.hua.dit.rentalapp.repositories;
 
 import gr.hua.dit.rentalapp.entities.PropertyVisit;
+import gr.hua.dit.rentalapp.enums.VisitStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -11,4 +13,10 @@ public interface PropertyVisitRepository extends JpaRepository<PropertyVisit, Lo
     List<PropertyVisit> findByPropertyId(Long propertyId);
     List<PropertyVisit> findByTenantId(Long tenantId);
     List<PropertyVisit> findByLandlordId(Long landlordId);
+    List<PropertyVisit> findByVisitStatus(VisitStatus status);
+    List<PropertyVisit> findByVisitDateBetween(Date startDate, Date endDate);
+    List<PropertyVisit> findByPropertyIdAndVisitStatus(Long propertyId, VisitStatus status);
+    List<PropertyVisit> findByTenantIdAndVisitStatus(Long tenantId, VisitStatus status);
+    List<PropertyVisit> findByLandlordIdAndVisitStatus(Long landlordId, VisitStatus status);
+    List<PropertyVisit> findByPropertyIdAndVisitDateBetween(Long propertyId, Date startDate, Date endDate);
 }
